c9fe30fa1f69758d0fc4d099881b7ec47c4ae3dd
manuvaladares <manuella.valadares@hotmail.com>
2025-10-15 08:45:11 -0300

    feat: adiciona o schema para matrícula

---
+++ app/matricula/schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from ..model import StatusAprovacaoEnum
+
+class MatriculaCreate(BaseModel):
+    id_aluno: int
+    id_turma: int
+
+class MatriculaResponse(BaseModel):
+    id_aluno: int
+    id_turma: int
+    status: StatusAprovacaoEnum | None = None 
+    nota_final: float | None = None
+
+    class Config:
+        from_attributes = True
