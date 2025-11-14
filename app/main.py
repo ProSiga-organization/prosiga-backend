5d596733348f67ab9e4443852ba4e82845713660
manuvaladares <manuella.valadares@hotmail.com>
2025-11-14 17:31:45 -0300

    fix: adiciona origens para o middleware CORS

--- app/main.py
+++ app/main.py
@@ -18,6 +18,8 @@ app = FastAPI(title="PróSiga API", description="API do PróSiga.")
 
 origins = [
     "http://localhost:3000",
+    "http://0.0.0.0:3000",
+    "http://127.0.0.1:3000",
 ]
 
 # Adiciona o Middleware
